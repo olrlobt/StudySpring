34cf69d2fee96ae450c4d6612af87eb755ec2838
olrlobt <317tmdgjs@naver.com>
2023-05-25 14:02:23 +0900

    test: 멤버 서비스 테스트 추가

---
+++ src/test/java/study/studyspring/service/MemberServiceTest.java
@@ -0,0 +1,69 @@
+package study.studyspring.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import study.studyspring.domain.Member;
+import study.studyspring.repository.MemoryMemberRepository;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+
+    }
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("Spring");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+
+    @Test
+    void 중복_회원_예외() {
+        Member member1 = new Member();
+        member1.setName("Spring");
+
+        Member member2 = new Member();
+        member2.setName("Spring");
+
+        memberService.join(member1);
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> memberService.join(member2));
+
+    }
+
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
