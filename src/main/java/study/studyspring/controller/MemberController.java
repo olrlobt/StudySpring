35fb532d0d5669da6859e25f35349484634cbc99
olrlobt <317tmdgjs@naver.com>
2023-05-29 17:42:55 +0900

    feat: 회원 가입, 조회 기능 추가

--- src/main/java/study/studyspring/controller/MemberController.java
+++ src/main/java/study/studyspring/controller/MemberController.java
@@ -1,7 +1,12 @@
 package study.studyspring.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import study.studyspring.domain.Member;
 import study.studyspring.service.MemberService;
 
 @Controller
@@ -13,4 +18,29 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMembersForm";
+
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
+    }
+
 }
