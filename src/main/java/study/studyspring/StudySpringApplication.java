3f94b33c28042e2b5629789a12b774b4339bf7ef
olrlobt <317tmdgjs@naver.com>
2023-05-29 18:47:02 +0900

    feat: JDBC 연결

--- src/main/java/study/studyspring/StudySpringApplication.java
+++ src/main/java/study/studyspring/StudySpringApplication.java
@@ -13,12 +13,5 @@ public class StudySpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudySpringApplication.class, args);
 
-		final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfig.class);
-		final MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
-		final MemberService memberService2 = applicationContext.getBean("memberService", MemberService.class);
-		System.out.println(memberService);
-		System.out.println(memberService2);
-		System.out.println(applicationContext.isSingleton("memberService"));
-
 	}
 }
--- src/main/java/study/studyspring/service/SpringConfig.java
+++ src/main/java/study/studyspring/service/SpringConfig.java
@@ -1,23 +1,32 @@
 package study.studyspring.service;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import study.studyspring.repository.JdbcMemberRepository;
 import study.studyspring.repository.MemberRepository;
 import study.studyspring.repository.MemoryMemberRepository;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
-    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public MemberService memberService(){
         return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
