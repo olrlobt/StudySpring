c11d44880682069d968efdfb42153cc1d874eabc
olrlobt <317tmdgjs@naver.com>
2024-01-02 22:43:50 +0900

    feat: Http 요청 - @ModelAttribute

--- 2. Spring MVC/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ 2. Spring MVC/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,12 +1,14 @@
 package hello.springmvc.basic.request;
 
 
+import hello.springmvc.basic.HelloData;
 import java.io.IOException;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -76,4 +78,23 @@ public class RequestParamController {
 
         return "ok";
     }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAtrributeV1(@ModelAttribute HelloData helloData) {
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData={}" , helloData);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAtrributeV2(HelloData helloData) {
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData={}" , helloData);
+        return "ok";
+    }
+
 }
