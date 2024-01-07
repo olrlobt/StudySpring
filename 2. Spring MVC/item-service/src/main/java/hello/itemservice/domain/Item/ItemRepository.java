05967dd65dcbce2a637f068d1edd58f523f5a43a
olrlobt <317tmdgjs@naver.com>
2024-01-07 18:45:07 +0900

    feat: 상품 도메인 개발

---
+++ 2. Spring MVC/item-service/src/main/java/hello/itemservice/domain/Item/ItemRepository.java
@@ -0,0 +1,43 @@
+package hello.itemservice.domain.Item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>(); // static
+    private static long sequence = 0L; //static
+
+    // 멀티스레드 환경을 고려하여, 실무에서는 HashMap 대신 ConcurrentHashMap을 사용해야 한다.
+    // sequence도 마찬가지.
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
