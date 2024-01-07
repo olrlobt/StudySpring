d8b478414767b6c774aa37d204375a7dd9374ffd
olrlobt <317tmdgjs@naver.com>
2024-01-07 21:43:07 +0900

    feat: 상품 상세

--- 2. Spring MVC/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ 2. Spring MVC/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,11 +26,20 @@ public class BasicItemController {
         return "basic/items";
     }
 
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable("itemId") long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+
     /**
      * 테스트용 데이터 추가
      */
     @PostConstruct
-    public void init(){
+    public void init() {
+
         itemRepository.save(new Item("itemA", 100, 10));
         itemRepository.save(new Item("itemB", 200, 20));
     }
