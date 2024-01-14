a5ed4236d4542d23b7fc9e2b07fa1babafe49d49
olrlobt <317tmdgjs@naver.com>
2024-01-14 23:47:07 +0900

    feat: 연관관계 메서드 추가, 지연로딩 적용

--- 3. Spring JPA/jpa/src/main/java/jpabook/jpashop/domain/Order.java
+++ 3. Spring JPA/jpa/src/main/java/jpabook/jpashop/domain/Order.java
@@ -18,16 +18,16 @@ public class Order {
     private Long id;
 
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -35,5 +35,22 @@ public class Order {
     private LocalDateTime orderDate; // 주문시간
 
     @Enumerated(EnumType.STRING)
-    private OrderStatus orderStatus; //주문상태 [ORDER,CANCEL]
+    private OrderStatus status; //주문상태 [ORDER,CANCEL]
+
+
+    // 연관관계 메서드
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
