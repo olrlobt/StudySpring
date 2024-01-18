84b7df7ff90679b2e24b0aaf91b6f1e4518365bc
olrlobt <317tmdgjs@naver.com>
2024-01-19 08:34:33 +0900

    feat: Order service

--- 3. Spring JPA/jpa/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ 3. Spring JPA/jpa/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,12 +2,15 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -25,6 +28,7 @@ public class OrderItem {
 
     private int orederPrice; // 주문 가격
     private int count; // 주문 수량
+
 
     //-- 생성 메서드 --//
     public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
