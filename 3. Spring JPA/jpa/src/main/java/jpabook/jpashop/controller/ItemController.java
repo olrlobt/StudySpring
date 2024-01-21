09c7835048381d1f8f525d4d4288525c82698e4d
olrlobt <317tmdgjs@naver.com>
2024-01-22 08:43:37 +0900

    feat: 상품 목록

--- 3. Spring JPA/jpa/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ 3. Spring JPA/jpa/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -34,4 +34,11 @@ public class ItemController {
         itemService.saveItem(book);
         return "redirect:/";
     }
+
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        model.addAttribute("items", itemService.findItems());
+        return "items/itemList";
+    }
 }
