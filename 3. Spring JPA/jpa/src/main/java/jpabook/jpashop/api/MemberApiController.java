2727d5464040c78fc351afad284b7d3af5f90c03
olrlobt <317tmdgjs@naver.com>
2024-03-02 21:40:26 +0900

    feat: 회원 조회 API-v2

--- 3. Spring JPA/jpa/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ 3. Spring JPA/jpa/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.api;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,28 @@ public class MemberApiController {
 	@GetMapping("/api/v1/members")
 	public List<Member> membersV1(){
 		return memberService.findMembers();
+	}
+
+	@GetMapping("/api/v2/members")
+	public Result memberV2(){
+		List<Member> findMembers = memberService.findMembers();
+		List<MemberDto> collect = findMembers.stream()
+			.map(m -> new MemberDto(m.getName()))
+			.collect(Collectors.toList());
+
+		return new Result(collect);
+	}
+
+	@Data
+	@AllArgsConstructor
+	static class Result<T> {
+		private T data;
+	}
+
+	@Data
+	@AllArgsConstructor
+	static class MemberDto{
+		private String name;
 	}
 
 	@PostMapping("/api/v1/members")
@@ -78,4 +101,6 @@ public class MemberApiController {
 		private Long id;
 		private String name;
 	}
+
+
 }
